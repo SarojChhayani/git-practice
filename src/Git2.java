2abdd605622a0ed20ed362f835353bb2ae0bfe0f
Saroj Chhayani <saroj.n.chhayani@gmail.com>
2024-03-09 16:03:01 +0000

    added branch 2

---
+++ src/Git2.java
@@ -0,0 +1,11 @@
+public class Git2 {
+    static int a = 5;
+    static int b = 5;
+    static int ans = a + b;
+
+    public static void main(String[] args) {
+        System.out.println("5+5 =" + ans);
+
+
+    }
+}
